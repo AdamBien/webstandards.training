71e3eb070378d861c9e34492dc940b76c21d3732
Adam Bien <abien@adam-bien.com>
2017-04-22 15:50:26 +0200

    custom events are fired by div

--- hello/src/app.js
+++ hello/src/app.js
@@ -1,19 +1,11 @@
-let textInput = document.querySelector("input[type=text]");
-console.dir(textInput);
+let div = document.querySelector("div");
+let customFire = new CustomEvent("fire", {
+    detail: {
+        type: "hot",
+        prio:42
+    }
+});
 
-textInput.addEventListener("keypress", e => console.log(e.target.value));
+div.addEventListener("fire", e => console.log(e));
 
-let buttonInput = document.querySelector("input[type=button]");
-console.dir(buttonInput);
-
-let button = document.querySelector("button");
-console.dir(button);
-
-let select = document.querySelector("select");
-console.dir(select);
-
-let progress = document.querySelector("progress");
-console.dir(progress);
-
-let meter = document.querySelector("meter");
-console.dir(meter);
+div.dispatchEvent(customFire);
