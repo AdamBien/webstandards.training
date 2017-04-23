80ee817fe9574e26e36ca59e33021f998d4005ba
Adam Bien <abien@adam-bien.com>
2017-04-23 08:00:59 +0200

    On load even listener implemented

--- hello/src/app.js
+++ hello/src/app.js
@@ -7,3 +7,6 @@ div.removeEventListener("click", first);
 
 div.addEventListener("click", e => console.log("second", e));
 div.addEventListener("click", e => console.log("third", e));
+
+window.addEventListener("load", e => console.log("load", e));
+window.onunload = e => console.log("unload",e);
