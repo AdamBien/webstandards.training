b2ad182aea232cf6e0df71ebc2aedd4b4a1866b6
Adam Bien <abien@adam-bien.com>
2017-04-25 12:09:49 +0200

    implementing data binding

--- hello/src/app.js
+++ hello/src/app.js
@@ -1,8 +1,7 @@
 let div = document.querySelector("div");
 let color = document.querySelector("input[type=color]");
-color.setAttribute("data-message", "hello world");
-let attributeValue = color.getAttribute("data-message");
-console.log(color.dataset.message);
-console.log(color.dataset);
+console.log(div.style);
+color.onchange = e => div.style.backgroundColor = e.target.value;
 
-console.log(...color.attributes);
+let text = document.querySelector("input[type=text]");
+text.onkeypress = e => div.innerText = e.target.value;
