bfe9e4338938b43bbc3a18b0b4436a48879ccf14
Adam Bien <abien@adam-bien.com>
2017-04-25 11:53:32 +0200

    attaching state with data attributes

--- hello/src/app.js
+++ hello/src/app.js
@@ -1,29 +1,8 @@
 let div = document.querySelector("div");
-
-let first = e => console.log("first", e);
-
-div.addEventListener("click", first);
-div.removeEventListener("click", first);
-
-div.addEventListener("click", e => console.log("second", e));
-div.addEventListener("click", e => console.log("third", e));
-
-window.addEventListener("load", e => console.log("load", e));
-window.onunload = e => console.log("unload", e);
-
-let text = document.querySelector("input[type=text]");
-console.dir(text);
-text.value = "hey duke";
-
-let button = document.querySelector("input[type=button]");
-button.value = "push me";
-
 let color = document.querySelector("input[type=color]");
-color.onchange = e => console.log(e.target.value);
-color.value = "#ffffff";
+color.setAttribute("data-message", "hello world");
+let attributeValue = color.getAttribute("data-message");
+console.log(color.dataset.message);
+console.log(color.dataset);
 
-let time = document.querySelector("input[type=time]");
-time.value = "13:13";
-//time.setAttribute("value", "11:12");
-let value = time.getAttribute("value");
-console.log("property",value);
+console.log(...color.attributes);
