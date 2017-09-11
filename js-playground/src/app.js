5a890ef92b44ddbb88db987ba639aa99ac3bb543
Adam Bien <abien@adam-bien.com>
2017-09-11 15:46:55 +0200

    undefined vs. null

--- js-playground/src/app.js
+++ js-playground/src/app.js
@@ -1,15 +1,23 @@
 class App { 
-    constructor() { 
+    constructor(message='duke') { 
+        this.message = message;
         console.log('created',this);
     }
 
     init() {
-        console.log('initialized');
+        console.log('initialized',this.message);
     }
 }
 
 const app = new App();
 app.init();
+
+
+const withParams = new App('java');
+withParams.init();
+
+const undef = new App('');
+undef.init();
 
 
 
