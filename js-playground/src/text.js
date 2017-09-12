85f45fd8b29aa95d99ce77e3aa5951956999a9be
Adam Bien <abien@adam-bien.com>
2017-09-12 08:20:58 +0200

    introducing static methods

--- js-playground/src/text.js
+++ js-playground/src/text.js
@@ -1,9 +1,16 @@
 export default class Text { 
     constructor(id) { 
-        this.domElement = document.querySelector(`#${id}`);
+        this.domElement = Text.lookup(id);
+    }
+
+    static lookup(id) { 
+        return document.querySelector(`#${id}`);
     }
 
     content(text='not set') { 
         this.domElement.innerText = text;
     }
 }
+
+const second = Text.lookup('second');
+console.dir(second);
