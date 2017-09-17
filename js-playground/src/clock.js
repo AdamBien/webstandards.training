ea4407b6317ab7ae3a59eb0dd37537d09b1d452a
Adam Bien <abien@adam-bien.com>
2017-09-17 09:53:46 +0200

    fat arrows as listeners

--- js-playground/src/clock.js
+++ js-playground/src/clock.js
@@ -4,7 +4,16 @@ export default class Clock {
         this.listener = listener;
         this.socket = new WebSocket('ws://localhost:8080/answers/clock');
         this.socket.onopen = e => console.log(e);
-        this.socket.onmessage = ({ data:event }) => this.listener(event);
+        
+        this.onTimeArrival = ({ data: event }) => {
+            console.log(event);
+            this.listener(event)
+        };
+
+        this.socket.onmessage = this.onTimeArrival;
+        
     }
+
+
 
 }
